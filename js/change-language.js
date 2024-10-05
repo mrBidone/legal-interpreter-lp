094a306c691e46acfe4b2abfaa221e0e076efc26
Andrey Bidenko <79172596+mrBidone@users.noreply.github.com>
2024-10-05 20:22:32 +0200

    Update change-language.js

--- js/change-language.js
+++ js/change-language.js
@@ -31,26 +31,37 @@ function setInitialLanguage(translations) {
   const savedLang = localStorage.getItem("selectedLanguage");
   const browserLang = navigator.language.slice(0, 2);
 
-  let initialLang =
-    savedLang ||
-    (["ru", "cz", "uz"].includes(browserLang) ? browserLang : "en");
+  let initialLang;
+
+  // Проверка: если сохраненный язык отсутствует или не поддерживается
+  if (savedLang && translations.hasOwnProperty(savedLang)) {
+    initialLang = savedLang;
+  } else if (translations.hasOwnProperty(browserLang)) {
+    initialLang = browserLang;
+  } else {
+    initialLang = "en"; // Загружаем английский язык по умолчанию
+  }
 
   applyTranslation(initialLang, translations);
 }
 
 function applyTranslation(lang, translations) {
-  Object.keys(elements).forEach((key) => {
-    if (elements[key]) {
-      elements[key].textContent = translations[lang][key];
-    }
-  });
+  if (translations.hasOwnProperty(lang)) {
+    Object.keys(elements).forEach((key) => {
+      if (elements[key] && translations[lang].hasOwnProperty(key)) {
+        elements[key].textContent = translations[lang][key];
+      }
+    });
 
-  document
-    .querySelectorAll(".language-btn")
-    .forEach((btn) => btn.classList.remove("isActiveLangBtn"));
-  document
-    .querySelector(`.language-btn[data-lang="${lang}"]`)
-    .classList.add("isActiveLangBtn");
+    document
+      .querySelectorAll(".language-btn")
+      .forEach((btn) => btn.classList.remove("isActiveLangBtn"));
+    document
+      .querySelector(`.language-btn[data-lang="${lang}"]`)
+      .classList.add("isActiveLangBtn");
 
-  localStorage.setItem("selectedLanguage", lang);
+    localStorage.setItem("selectedLanguage", lang);
+  } else {
+    console.error(`Ошибка: Перевод для языка "${lang}" не найден.`);
+  }
 }
